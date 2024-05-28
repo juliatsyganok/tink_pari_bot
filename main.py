0efb6de34201a05eb97532fff4b98845c76588de
Student <julia.tsyganok.9@yandex.ru>
2024-05-28 12:42:35 +0300

    init

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from keyboard_helper import get_keyboard, keyboards
 import pari_serves as ps
 
 import storage.user_repository as user_storage
+import storage.pari_repository as pari_storage
 
 
 storage = MemoryStorage()
@@ -42,15 +43,19 @@ async def start(message: types.Message, state: FSMContext):
 
 @dp.message(StateFilter(UserStates.CREATE_PARI))
 async def set_pari_name(message: types.Message, state: FSMContext):
+    pari_storage.add_pari(message.text, message.from_user.username)
     text = ps.set_pari_taker()
     await message.answer(text)
     await state.set_state(UserStates.SETTING_PARI_TAKER)
 
 @dp.message(StateFilter(UserStates.SETTING_PARI_TAKER))
-async def set_pari_name(message: types.Message, state: FSMContext):
+async def set_pari_taker(message: types.Message, state: FSMContext):
+    pari = pari_storage.set_pari_taker(message.from_user.username, message.text)
     text = ps.pari_created()
+    taker_text = 'Пользователь' + pari.challenger_name + ' заключил с вами пари' + pari.name
     await message.answer(text)
     await state.set_state(UserStates.BASE)
+    await Bot.send_message(user_storage.get_user(pari.taker_name), taker_text)
 
 async def main():
     await dp.start_polling(Bot)
